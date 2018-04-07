f96e3c153eff405ccf5cb89afc7cb2a003a8779a
Rashmit Pankhania <RashmitPankhania@gmail.com>
2018-04-07 11:33:35 +0530

    added index.ejs

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-	res.send('Hello world')
+	res.render('index');
 })
 
 app.listen(3000, function () {
 	// body...
 	console.log('App running on port 3000')
 })
+
+app.set('view engine', 'ejs')
