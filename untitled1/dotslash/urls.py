06e3e49a70e2d55ea9f4c531ad3c06179c43456c
Rashmit Pankhania <RashmitPankhania@gmail.com>
2018-04-08 10:04:53 +0530

    django changes

--- untitled1/dotslash/urls.py
+++ untitled1/dotslash/urls.py
@@ -6,5 +6,8 @@ app_name = 'dotslash'
 urlpatterns = [
     url(r'^home/$', views.home, name='home'),
     url(r'narmad/$', views.narmad, name='narmad'),
-    url(r'registration/$', views.registration, name='register')
+    url(r'registration/$', views.registration, name='register'),
+    url(r'^canteen/$', views.canteen, name='canteen'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^council/$', views.council, name='council'),
 ]
--- untitled1/dotslash/views.py
+++ untitled1/dotslash/views.py
@@ -21,18 +21,28 @@ def narmad(request):
     context = {
         'form': myform,
     }
-    return render(request, 'reg_suc.html', context)
+    return render(request, 'narmad.html', context)
 
 def registration(request):
-    myform = RegForm()
-    if request.method == "POST":
-        form = RegForm(request.POST)
-        if form.is_valid():
-            myform = form.cleaned_data
-
-    else:
-        form = RegForm()
     context = {
-        'form': myform,
+
     }
     return render(request, 'reg_suc.html', context)
+
+def canteen(request):
+    context = {
+
+    }
+    return render(request, 'canteen.html', context)
+
+def cart(request):
+    context = {
+
+    }
+    return render(request, 'cart.html', context)
+
+def council(request):
+    context = {
+
+    }
+    return render(request, 'council.html', context)
