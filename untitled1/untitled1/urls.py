c9db61c28343e83f4ba9116792577ef64a945995
Rashmit Pankhania <RashmitPankhania@gmail.com>
2018-04-08 13:49:33 +0530

    canteen changes

--- untitled1/untitled1/urls.py
+++ untitled1/untitled1/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-from dotslash import urls
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dotslash/', include('dotslash.urls'),)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
